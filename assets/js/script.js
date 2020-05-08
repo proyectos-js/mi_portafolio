57c9ed541444ca64d330dc9465aae0ac5bf16f74
proyectos-js <github.proyectos.js@gmail.com>
2020-05-07 23:20:23 -0400

    new version-4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+function() {
+    $("a").click(function (e) {
+        e.preventDefault();
+    });
+
+}
+
+$(function ("a") {
+    $("a").click(function (e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            var url = this.hash;
+            $('html,body').animate({
+                scrollTop: $(url).offset().top
+            }, 800, function () {
+                window.location.hash = url;
+            });
+        }
+    });
+});
+
+
